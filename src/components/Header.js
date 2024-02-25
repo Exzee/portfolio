dc6d61777dddd6c3dc797147e46f190e9b0eda80
Exzee <madankoshi@gmail.com>
2024-02-25 15:03:26 +0700

    First Commit: Landing Page Portfolio

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
-
+import React from "react";
+// images
+import Logo from "../assets/logo_cp.svg";
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header className="py-8">
+      <div className="container mx-auto">
+        <div className="flex justify-between items-center">
+          {/* Logo */}
+          <a href="#">
+            <img src={Logo} alt="Logo" />
+          </a>
+          {/* Button */}
+          <button className="btn btn-sm">Work with me</button>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
